b573a8e6e60e0056e9ed38039ce8902fdda7818c
vovan <vlbakulev@gmail.com>
2021-05-15 16:23:01 +0300

    change tests

--- ml_project/tests/conftest.py
+++ ml_project/tests/conftest.py
@@ -31,7 +31,7 @@ LIST_TARGET = [0, 1]
 
 
 @pytest.fixture()
-def train_dataset_path(tmpdir):
+def dataset_path(tmpdir):
     random.seed(SEED)
     df = pd.DataFrame({
         "age": [random.randint(MIN_AGE, MAX_AGE) for _ in range(DATASET_SIZE)],
@@ -50,30 +50,6 @@ def train_dataset_path(tmpdir):
         "target": [random.choice(LIST_TARGET) for _ in range(DATASET_SIZE)],
     })
     dir_csv = os.path.join(tmpdir, "train_data_sample.csv")
-    df.to_csv(dir_csv, index=False)
-    return dir_csv
-
-
-@pytest.fixture()
-def predict_dataset_path(tmpdir):
-    random.seed(SEED)
-    df = pd.DataFrame({
-        "age": [random.randint(MIN_AGE, MAX_AGE) for _ in range(DATASET_SIZE)],
-        "sex": [random.choice(LIST_SEX) for _ in range(DATASET_SIZE)],
-        "cp": [random.choice(LIST_CP) for _ in range(DATASET_SIZE)],
-        "trestbps": [random.randint(MIN_TRESTBPS, MAX_TRESTBPS) for _ in range(DATASET_SIZE)],
-        "chol": [random.randint(MIN_CHOL, MAX_CHOL) for _ in range(DATASET_SIZE)],
-        "fbs": [random.choice(LIST_FBS) for _ in range(DATASET_SIZE)],
-        "restecg": [random.choice(LIST_RESTECG) for _ in range(DATASET_SIZE)],
-        "thalach": [random.randint(MIN_THALACH, MAX_THALACH) for _ in range(DATASET_SIZE)],
-        "exang": [random.choice(LIST_EXANG) for _ in range(DATASET_SIZE)],
-        "oldpeak": [random.uniform(MIN_OLDPEAK, MAX_OLDPEAK) for _ in range(DATASET_SIZE)],
-        "slope": [random.choice(LIST_SLOPE) for _ in range(DATASET_SIZE)],
-        "ca": [random.choice(LIST_CA) for _ in range(DATASET_SIZE)],
-        "thal": [random.choice(LIST_THAL) for _ in range(DATASET_SIZE)],
-        "target": [0 for _ in range(DATASET_SIZE)],
-    })
-    dir_csv = os.path.join(tmpdir, "predict_data_sample.csv")
     df.to_csv(dir_csv, index=False)
     return dir_csv
 
