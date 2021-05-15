96789dea3cbec595a45be01c08bce66ee4d5b25a
vovan <vlbakulev@gmail.com>
2021-05-15 15:34:03 +0300

    add predict

--- ml_project/src/data/make_dataset.py
+++ ml_project/src/data/make_dataset.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from typing import Tuple
 
 import pandas as pd
@@ -6,19 +9,26 @@ from sklearn.model_selection import train_test_split
 from src.entities import SplittingParams
 
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
 def read_data(path: str) -> pd.DataFrame:
+    logger.info(f"Reading dataset from {path}")
     data = pd.read_csv(path)
+    logger.info("Dataset read successfully!")
     return data
 
 
 def split_train_val_data(
     data: pd.DataFrame, params: SplittingParams
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """
 
-    :rtype: object
-    """
+    logger.info(f"Split datasets into training and validation parts. The validation part is {params.val_size * 100} percent")
     train_data, val_data = train_test_split(
         data, test_size=params.val_size, random_state=params.random_state
     )
+    logger.info(f"Dataset split successfully!")
     return train_data, val_data
---
+++ ml_project/src/entities/predict_pipeline_params.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from .feature_params import FeatureParams
+from marshmallow_dataclass import class_schema
+import yaml
+
+
+@dataclass()
+class PredictPipelineParams:
+    input_data_path: str
+    model_path: str
+    output_predict_path: str
+
+
+PredictPipelineParamsSchema = class_schema(PredictPipelineParams)
+
+
+def read_predict_pipeline_params(path: str) -> PredictPipelineParams:
+    with open(path, "r") as input_stream:
+        schema = PredictPipelineParamsSchema()
+        return schema.load(yaml.safe_load(input_stream))
